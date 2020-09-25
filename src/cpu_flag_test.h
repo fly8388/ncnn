15c676d27aa7db11b3f318bc745c3aae73bce10c
Restyled.io <commits@restyled.io>
2020-09-25 07:00:42 +0000

    Restyled by clang-format

--- src/cpu_flag_test.h
+++ src/cpu_flag_test.h
@@ -27,47 +27,49 @@
 #include <immintrin.h>
 
 namespace ncnn {
-enum class CpuFlag {
-	None = 0x0,
-	Initialized = 0x1,
-	// arm
-	ARM = 0x2,
-	NEON = 0x4,
-	// x86
-	X86 = 0x10,
-	SSE2 = 0x20,
-	SSSE3 = 0x40,
-	SSE41 = 0x80,
-	SSE42 = 0x100,
-	AVX = 0x200,
-	AVX2 = 0x400,
-	ERMS = 0x800,
-	FMA3 = 0x1000,
-	F16C = 0x2000,
-	GFNI = 0x4000,
-	AVX512BW = 0x8000,
-	AVX512VL = 0x10000,
-	AVX512VBMI = 0x20000,
-	AVX512VBMI2 = 0x40000,
-	AVX512VBITALG = 0x80000,
-	AVX512VPOPCNTDQ = 0x100000,
-	// mips
-	MIPS = 0x200000,
-	MSA = 0x400000,
-	MMI = 0x800000,
+enum class CpuFlag
+{
+    None = 0x0,
+    Initialized = 0x1,
+    // arm
+    ARM = 0x2,
+    NEON = 0x4,
+    // x86
+    X86 = 0x10,
+    SSE2 = 0x20,
+    SSSE3 = 0x40,
+    SSE41 = 0x80,
+    SSE42 = 0x100,
+    AVX = 0x200,
+    AVX2 = 0x400,
+    ERMS = 0x800,
+    FMA3 = 0x1000,
+    F16C = 0x2000,
+    GFNI = 0x4000,
+    AVX512BW = 0x8000,
+    AVX512VL = 0x10000,
+    AVX512VBMI = 0x20000,
+    AVX512VBMI2 = 0x40000,
+    AVX512VBITALG = 0x80000,
+    AVX512VPOPCNTDQ = 0x100000,
+    // mips
+    MIPS = 0x200000,
+    MSA = 0x400000,
+    MMI = 0x800000,
 };
 
-class CpuFlagTest {
+class CpuFlagTest
+{
 public:
-	CpuFlagTest ();
+    CpuFlagTest();
 
-	bool TestSupport (CpuFlag _flag);
+    bool TestSupport(CpuFlag _flag);
 
-	std::string GetSupportString ();
+    std::string GetSupportString();
 
 private:
-	CpuFlag m_cpu_info = CpuFlag::None;
+    CpuFlag m_cpu_info = CpuFlag::None;
 };
-}
+} // namespace ncnn
 
 #endif //NCNN_INTEL_INSTRUCTION_TEST_H
